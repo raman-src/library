c3893b45f455c2e297246d1ade34018e5e0fda5f
raman <ramanlfc2@gmail.com>
2020-08-12 18:27:34 +0530

    initial commit

---
+++ src/test/java/com/smu/rest/library/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smu.rest.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
